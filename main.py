5eadd9b38ba081236ac3cc770912c6b4116a799f
CRCDApp <11474624+CRCDApp@users.noreply.github.com>
2019-11-03 22:30:29 +0100

    number of similar words updated

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ path = create_guardians_dataset(path='./', articleCount=40000, category='world')
 # Creating Word2Vec model
 model = clean_data_and_create_model(dataPath=path, modelPath='./MyModel.model')
 # Loading model and getting similar words
-similarwords, distances = read_model_and_get_similar_words(modelPath='./MyModel.model', numberOfSimilarWords=3,
+similarwords, distances = read_model_and_get_similar_words(modelPath='./MyModel.model', numberOfSimilarWords=100,
                                                            targetWords=targetwords)
 # Print(similarwords)
 print_similarity_table(targetwords, similarwords, distances)
